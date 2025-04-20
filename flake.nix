ac24d952b262dad597048530acabff1079cbb635
mochouaaaaa <nn.mochou+github@gmail.com>
2025-04-21 07:07:53 +0800

    add fastfetch config

--- flake.nix
+++ flake.nix
@@ -11,13 +11,13 @@
       "https://cache.nixos.org"
       "https://mirrors.ustc.edu.cn/nix-channels/store"
 
-      "https://yazi.cachix.org"
+      # "https://yazi.cachix.org"
       "https://nix-gaming.cachix.org"
     ];
     extra-trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
 
-      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+      # "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
       "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
     ];
   };
--- modules/home/base/tools/programs/fastfetch.nix
+++ modules/home/base/tools/programs/fastfetch.nix
@@ -1,7 +1,153 @@
 {
+  pkgs,
+  lib,
+  isNixos,
+  ...
+}:
+let
+  image = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/JaKooLit/NixOS-Hyprland/main/assets/fastfetch/nixos.png";
+    hash = "sha256-MG4t8KvfrBowZzLpkkl2EYWX5KqS6wxHSNrTUnMQaaM=";
+  };
+in
+{
   programs = {
     fastfetch = {
       enable = true;
     };
   };
+
+  xdg.configFile."fastfetch/config.jsonc".text = ''
+    {
+    "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
+    "logo": {
+      ${lib.optionalString (isNixos) ''"source": "${image}",''}
+      "type": "kitty-direct",
+      "height": 18,
+      "width": 36,
+      "padding": {
+        "top": 1
+        }
+      },
+    "display": {
+    "separator": " 󰑃  "
+    },
+    "modules": [
+        "break",
+        {
+        "type": "os",
+        "key": " DISTRO",
+        "keyColor": "yellow"
+        },
+        {
+        "type": "kernel",
+        "key": "│ ├",
+        "keyColor": "yellow"
+        },
+        {
+        "type": "packages",
+        "key": "│ ├󰏖",
+        "keyColor": "yellow"
+        },
+        {
+        "type": "shell",
+        "key": "│ └",
+        "keyColor": "yellow"
+        },
+        {
+        "type": "wm",
+        "key": " DE/WM",
+        "keyColor": "blue"
+        },
+        {
+        "type": "wmtheme",
+        "key": "│ ├󰉼",
+        "keyColor": "blue"
+        },
+        {
+        "type": "icons",
+        "key": "│ ├󰀻",
+        "keyColor": "blue"
+        },
+        {
+            "type": "cursor",
+            "key": "│ ├",
+            "keyColor": "blue",
+        },
+        {
+            "type": "terminalfont",
+            "key": "│ ├",
+            "keyColor": "blue",
+        },
+        {
+        "type": "terminal",
+        "key": "│ └",
+        "keyColor": "blue"
+        },
+        {
+        "type": "host",
+        "key": "󰌢 SYSTEM",
+        "keyColor": "green"
+        },
+        {
+        "type": "cpu",
+        "key": "│ ├󰻠",
+        "keyColor": "green"
+        },
+        {
+        "type": "gpu",
+        "key": "│ ├󰻑",
+        "format": "{2}",
+        "keyColor": "green"
+        },
+        {
+                "type": "display",
+        "key": "│ ├󰍹",
+        "keyColor": "green",
+        "compactType": "original-with-refresh-rate"
+        },
+        {
+        "type": "memory",
+        "key": "│ ├󰾆",
+        "keyColor": "green"
+        },
+        {
+        "type": "swap",
+        "key": "│ ├󰓡",
+        "keyColor": "green"
+        },
+        {
+        "type": "uptime",
+        "key": "│ ├󰅐",
+        "keyColor": "green"
+        },
+        {
+        "type": "display",
+        "key": "│ └󰍹",
+        "keyColor": "green"
+        },
+        {
+        "type": "sound",
+        "key": " AUDIO",
+        "format": "{2}",
+        "keyColor": "magenta"
+        },
+        {
+        "type": "player",
+        "key": "│ ├󰥠",
+        "keyColor": "magenta"
+        },
+        {
+        "type": "media",
+        "key": "│ └󰝚",
+        "keyColor": "magenta"
+        },
+        {
+        "type": "custom",
+        "format": "\u001b[90m  \u001b[31m  \u001b[32m  \u001b[33m  \u001b[34m  \u001b[35m  \u001b[36m  \u001b[37m  \u001b[38m  \u001b[39m  \u001b[39m    \u001b[38m  \u001b[37m  \u001b[36m  \u001b[35m  \u001b[34m  \u001b[33m  \u001b[32m  \u001b[31m  \u001b[90m "
+        },
+        "break",
+        ]
+    }
+  '';
 }
