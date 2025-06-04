4120198fcbfe594978daa57a1bc58a27a99c4eb3
mochouaaaaa <nn.mochou+github@gmail.com>
2025-06-04 23:07:18 +0800

    feat(xdg): add rust config

--- modules/home/base/core/xdg.nix
+++ modules/home/base/core/xdg.nix
@@ -1,10 +1,15 @@
 { config, ... }:
-{
+rec {
   xdg = {
     enable = true;
     cacheHome = "${config.home.homeDirectory}/.cache";
     configHome = "${config.home.homeDirectory}/.config";
     dataHome = "${config.home.homeDirectory}/.local/share";
     stateHome = "${config.home.homeDirectory}/.local/state";
+  };
+
+  home.sessionVariables = {
+    CARGO_HOME = "${xdg.dataHome}/cargo";
+    RUSTUP_HOME = "${xdg.dataHome}/rustup";
   };
 }
