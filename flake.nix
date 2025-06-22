7eda7d6b2a3e5b0c7cf42e5a4fd98b2d79b31d59
mochouaaaaa <nn.mochou+github@gmail.com>
2025-06-23 07:23:51 +0800

    feat(flake.lock): update lock file

--- flake.nix
+++ flake.nix
@@ -92,6 +92,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
 
     # desktop components
