533d023ce4acbc96d29ee026f411d73c1affefed
mochouaaaaa <nn.mochou+github@gmail.com>
2025-07-04 02:37:16 +0800

    feat(firefox): add plugin nopecha

--- modules/home/base/tools/programs/firefox/plugins/nope-cha.nix
+++ modules/home/base/tools/programs/firefox/plugins/nope-cha.nix
@@ -16,7 +16,7 @@
                 version = "0.4.13";
                 addonId = "{2f67aecb-5dac-4f76-9378-0ac4f2bedc9c}";
                 url = "https://addons.mozilla.org/firefox/downloads/file/4393222/noptcha-${version}.xpi";
-                sha256 = "";
+                sha256 = "sha256-HvH8p8dcxIcUaASav/HM7YblD3dHJxCWRbXWDywBP6M=";
                 meta = with lib; {
                   homepage = "https://www.fehelper.com";
                   description = "AI Solver for reCAPTCHA, FunCAPTCHA, and all CAPTCHA.";
