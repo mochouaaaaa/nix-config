8863203a2632cc88d0b3bb009e6f69e78f2702a0
mochouaaaaa <nn.mochou+github@gmail.com>
2025-07-04 02:36:50 +0800

    feat(nvim): add pynvim packages

--- modules/home/base/tools/programs/neovim/default.nix
+++ modules/home/base/tools/programs/neovim/default.nix
@@ -39,32 +39,11 @@
         pkgs.sqlite
         pkgs.libgit2
       ];
-      extraPython3Packages = ps: [ ps.debugpy ];
+      extraPython3Packages = ps: [
+        ps.debugpy
+        ps.pynvim
+      ];
       withNodeJs = true;
-      # These environment variables are needed to build and run binaries
-      # with external package managers like mason.nvim.
-      #
-      # LD_LIBRARY_PATH is also needed to run the non-FHS binaries downloaded by mason.nvim.
-      # it will be set by nix-ld, so we do not need to set it here again.
-      # extraWrapperArgs = with pkgs; [
-      #   "--suffix"
-      #   "LIBRARY_PATH"
-      #   ":"
-      #   "${lib.makeLibraryPath [
-      #     stdenv.cc.cc
-      #     zlib
-      #   ]}"
-      #
-      #   # PKG_CONFIG_PATH is used by pkg-config before compilation to search directories
-      #   # containing .pc files that describe the libraries that need to be linked to your program.
-      #   "--suffix"
-      #   "PKG_CONFIG_PATH"
-      #   ":"
-      #   "${lib.makeSearchPathOutput "dev" "lib/pkgconfig" [
-      #     stdenv.cc.cc
-      #     zlib
-      #   ]}"
-      # ];
     };
   };
 
