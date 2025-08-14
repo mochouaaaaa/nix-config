d90750affed9e206c05c8db61fc8852758a6e36d
mochou <nn.mochou+github@gmail.com>
2025-08-14 13:49:28 +0800

    update(desktop): kde auto dark/light theme

--- modules/home/linux/desktop/window/kde/packages/themes.nix
+++ modules/home/linux/desktop/window/kde/packages/themes.nix
@@ -12,9 +12,42 @@ in
 
     qt.style.name = "kvantum";
 
+    home.packages = with pkgs; [
+      whitesur-kde
+      (writeShellScriptBin "switch-theme" ''
+        theme=$1
+
+        export XDG_CURRENT_DESKTOP=KDE
+        export KDE_FULL_SESSION=true
+
+        if [[ -z "$DBUS_SESSION_BUS_ADDRESS" ]]; then
+          export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u)/bus"
+        fi
+
+        if [[ $theme == "light" ]]; then
+          lookandfeeltool -a com.github.vinceliuice.WhiteSur-alt
+        elif [[ $theme == "dark" ]]; then
+          lookandfeeltool -a com.github.vinceliuice.WhiteSur-dark
+        fi
+
+      '')
+    ];
+
     modules'.themes.auto = {
       enable = true;
-      kdeTheme.enable = true;
+    };
+
+    services.darkman = {
+      lightModeScripts = {
+        kde-theme = ''
+          switch-theme light
+        '';
+      };
+      darkModeScripts = {
+        kde-theme = ''
+          switch-theme dark
+        '';
+      };
     };
 
   };
