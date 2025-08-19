3b572aad54ca8ccf1e3ffbdc0172677a68f31221
mochou <nn.mochou+github@gmail.com>
2025-08-20 01:59:41 +0800

    fix(hypridle): dmps timeout bug

--- modules/home/linux/desktop/window/hyprland/config/caelestia/hypridle.nix
+++ modules/home/linux/desktop/window/hyprland/config/caelestia/hypridle.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   config,
   ...
 }:
@@ -13,21 +14,33 @@ in
         general = {
           lock_cmd = lib.mkForce "caelestia shell lock lock";
         };
-        listener = lib.mkForce [
-          {
-            timeout = 600;
-            on-timeout = "loginctl lock-session";
-          }
-          {
-            timeout = 600;
-            on-timeout = "hyprctl dispatch dpms off";
-            on-resume = "hyprctl dispatch dpms on";
-          }
-          {
-            timeout = 1800;
-            on-timeout = "systemctl suspend-then-hibernate || loginctl suspend";
-          }
-        ];
+        listener =
+          let
+            lockscreen-dpms = pkgs.writeShellScriptBin "lockscreen-dpms" ''
+              LOCKED=$(loginctl show-session "$XDG_SESSION_ID" -p LockedHint | cut -d= -f2)
+              if [ "$LOCKED" = "yes" ]; then
+                  echo "🔒 已锁屏，允许息屏"
+                  hyprctl dispatch dpms off
+               else
+                  echo "🖥 未锁屏，不息屏"
+               fi
+            '';
+          in
+          lib.mkForce [
+            {
+              timeout = 600;
+              on-timeout = "loginctl lock-session";
+            }
+            {
+              timeout = 180;
+              on-timeout = "${lib.getExe lockscreen-dpms}";
+              on-resume = "hyprctl dispatch dpms on";
+            }
+            {
+              timeout = 1800;
+              on-timeout = "systemctl suspend-then-hibernate || loginctl suspend";
+            }
+          ];
       };
     };
   };
