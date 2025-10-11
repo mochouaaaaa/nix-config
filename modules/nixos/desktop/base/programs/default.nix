0d14d9dc696347122bbd26b64d091bd633ed159e
mochou <nn.mochou+github@gmail.com>
2025-10-12 07:33:09 +0800

    update(nixos): wshowkeys version

--- modules/nixos/desktop/base/programs/default.nix
+++ modules/nixos/desktop/base/programs/default.nix
@@ -1,6 +1,18 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   imports = lib.importModule' ./.;
 
-  programs.wshowkeys.enable = true;
+  programs.wshowkeys = rec {
+    enable = true;
+    package = pkgs.wshowkeys.overrideAttrs (finalAttrs: {
+      pname = "wshowkeys-last";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "ammgws";
+        repo = "wshowkeys";
+        tag = "0.1";
+        hash = "sha256-/HvNCQWsXOJZeCxHWmsLlbBDhBzF7XP/SPLdDiWMDC4=";
+      };
+    });
+  };
 }
