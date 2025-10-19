ad57f0ae2600ef7b069ca8af617e2c67ac2bab2c
mochou <nn.mochou+github@gmail.com>
2025-10-19 10:50:32 +0800

    chore: lock and versions

--- flake.nix
+++ flake.nix
@@ -124,7 +124,8 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell?ref=v2.18.2";
+      # url = "github:noctalia-dev/noctalia-shell?ref=v2.18.2";
+      url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     DankMaterialShell = {
