a5245784ccd3fa2eaada7d7e9e78aaa931bd1109
mochou <nn.mochou+github@gmail.com>
2025-10-19 10:48:41 +0800

    refactor(hm): neovim treesitter

--- modules/sharedModule/home-manager/tools/shell/neovim/plugins/treesitter.nix
+++ modules/sharedModule/home-manager/tools/shell/neovim/plugins/treesitter.nix
@@ -8,76 +8,78 @@
     tree-sitter
   ];
 
-  programs.nixvim.extraPackages = lib.mkBefore (
-    with pkgs.vimPlugins;
-    [
-      nvim-treesitter-textobjects
-      nvim-treesitter-parsers.regex
-
-      # git
-      nvim-treesitter-parsers.git_rebase
-      nvim-treesitter-parsers.gitignore
-      nvim-treesitter-parsers.gitcommit
-      nvim-treesitter-parsers.gitattributes
-      nvim-treesitter-parsers.git_config
-
-      # base
-      nvim-treesitter-parsers.bash
-      nvim-treesitter-parsers.diff
-      nvim-treesitter-parsers.comment
-      nvim-treesitter-parsers.vim
-      nvim-treesitter-parsers.dockerfile
-
-      # sql
-      nvim-treesitter-parsers.sql
-      nvim-treesitter-parsers.graphql
-
-      # c
-      nvim-treesitter-parsers.c
-      nvim-treesitter-parsers.cpp
-      nvim-treesitter-parsers.cmake
-      nvim-treesitter-parsers.swift
-      nvim-treesitter-parsers.qmljs
-
-      # lua
-      nvim-treesitter-parsers.lua
-      nvim-treesitter-parsers.luap
-
-      # golang
-      nvim-treesitter-parsers.go
-      nvim-treesitter-parsers.gomod
-      nvim-treesitter-parsers.gosum
-      nvim-treesitter-parsers.gowork
-
-      # python
-      nvim-treesitter-parsers.python
-
-      # rust
-      nvim-treesitter-parsers.rust
-
-      # javascript
-      nvim-treesitter-parsers.javascript
-      nvim-treesitter-parsers.typescript
-      nvim-treesitter-parsers.tsx
-      nvim-treesitter-parsers.vue
-      nvim-treesitter-parsers.scss
-      nvim-treesitter-parsers.css
-      nvim-treesitter-parsers.html
-      nvim-treesitter-parsers.astro
-
-      # markdown
-      nvim-treesitter-parsers.markdown
-      nvim-treesitter-parsers.markdown_inline
-      nvim-treesitter-parsers.latex
-
-      # filetype
-      nvim-treesitter-parsers.yaml
-      nvim-treesitter-parsers.json
-      nvim-treesitter-parsers.json5
-      nvim-treesitter-parsers.jsonc
-      nvim-treesitter-parsers.toml
-      nvim-treesitter-parsers.xml
-      nvim-treesitter-parsers.ini
-    ]
-  );
+  programs.nixvim = {
+    plugins.treesitter = {
+      enable = true;
+      grammarPackages = with pkgs.vimPlugins.nvim-treesitter.builtGrammars; [
+        regex
+        #
+        # # git
+        git_rebase
+        gitignore
+        gitcommit
+        gitattributes
+        git_config
+        #
+        # # base
+        bash
+        diff
+        comment
+        vim
+        dockerfile
+        #
+        # # sql
+        sql
+        graphql
+        #
+        # # c
+        c
+        cpp
+        cmake
+        swift
+        qmljs
+        #
+        # # lua
+        lua
+        luap
+        #
+        # # golang
+        go
+        gomod
+        gosum
+        gowork
+        #
+        # # python
+        python
+        htmldjango
+        #
+        # # rust
+        rust
+        #
+        # # javascript
+        javascript
+        typescript
+        tsx
+        vue
+        scss
+        css
+        html
+        astro
+        #
+        # # markdown
+        markdown
+        markdown_inline
+        latex
+        #
+        # # filetype
+        yaml
+        json
+        json5
+        jsonc
+        toml
+        xml
+        ini
+      ];
+    };
+  };
 }
