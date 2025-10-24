06ef354b93d86caf69bf11c6944694839cf2ba4e
mochou <nn.mochou+github@gmail.com>
2025-10-24 19:41:36 +0800

    feat(desktop): add gedit config

---
+++ modules/home/linux/desktop/window/share/gedit.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
+let
+  cfg = config.modules'.desktop;
+in
+{
+
+  config = lib.mkIf (cfg.hyprland.enable || cfg.niri.enable || cfg.gnome.enable) {
+
+    home.packages = [
+      pkgs.gedit
+    ];
+
+    dconf.settings = {
+      "org/gnome/gedit/preferences/editor" = {
+        auto-save = true;
+        display-right-margin = true;
+        editor-font = "Monaco Nerd Font Mono 12";
+        right-margin-position = lib.hm.gvariant.mkUint32 120;
+        tabs-size = lib.hm.gvariant.mkUint32 4;
+        use-default-font = false;
+        wrap-last-split-mode = "word";
+      };
+    };
+
+  };
+}
