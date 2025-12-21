a1dbaef71346ba8d053369630e8ca188f7d506c2
mochou <nn.mochou+github@gmail.com>
2025-12-21 10:15:43 +0800

    fix(wayland.windowManager.hyprland): 调整monitor配置以支持更高的bitdepth和颜色管理
    在monitor配置中，将monitor设置从",2560x1440@60,auto,1"更改为",2560x1440@60, auto, 1, bitdepth,10, cm,srgb"，以支持10位颜色深度和sRGB颜色管理。这样可以提高显示质量，特别是在色彩准确性要求较高的应用场景中。

--- modules/home/linux/desktop/window/hyprland/config/monitors.nix
+++ modules/home/linux/desktop/window/hyprland/config/monitors.nix
@@ -10,14 +10,13 @@ in
   config = lib.mkIf cfg.enable {
     wayland.windowManager.hyprland = {
       settings = {
-        monitor = ",2560x1440@60,auto,1";
+        monitor = ",2560x1440@60, auto, 1, bitdepth,10, cm,srgb";
         # monitor=",3840x2160@60,auto,1.4";
         #monitor = eDP-1, preferred, auto, 1
         #monitor = eDP-1, 2560x1440@165, 0x0, 1 #own screen
         #monitor = DP-3, 1920x1080@240, auto, 1
         #monitor = DP-1, preferred, auto, 1
         #monitor = HDMI-A-1, preferred,auto,1
-
       };
     };
   };
