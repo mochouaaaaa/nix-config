5066cf39f7bed8ea02f13a0483695996de778c36
mochou <nn.mochou+github@gmail.com>
2025-12-22 20:20:54 +0800

    fix(matugen): 修正终端配置文件路径
    在终端配置中，修正了kitty的主题配置文件路径，使其从 "include current-theme.conf" 更改为 "include themes/noctalia.conf"。这将确保kitty能够正确加载noctalia主题配置文件。
    
    此外，在ghostty的设置中，对使用noctalia主题和darkmaterial主题的条件进行了格式上的统一，确保代码格式的一致性，便于后续维护。

--- modules/home/linux/desktop/component/matugen/terminal.nix
+++ modules/home/linux/desktop/component/matugen/terminal.nix
@@ -14,25 +14,31 @@ in
     lib.mkMerge [
       {
         modules'.packages.terminal.kitty.extraConfig = lib.mkMerge [
-          (lib.mkIf useNoctaliaKitty (lib.mkAfter [ "include current-theme.conf" ]))
+
+          (lib.mkIf useNoctaliaKitty (lib.mkAfter [ "include themes/noctalia.conf" ]))
+
           (lib.mkIf useDarkMaterial (
             lib.mkAfter [
               "include dank-tabs.conf"
               "include dank-theme.conf"
             ]
           ))
+
         ];
       }
 
       {
         programs.ghostty.settings = lib.mkMerge [
+
           (lib.mkIf useNoctaliaGhostty {
             theme = "noctalia";
           })
+
           (lib.mkIf useDarkMaterial {
             theme = "dankcolors";
             app-notifications = "no-clipboard-copy,no-config-reload";
           })
+
         ];
       }
     ]
