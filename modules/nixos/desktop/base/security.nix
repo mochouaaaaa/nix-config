59ae067ed353d3f73cb0b1622fdae2461f0b0b18
mochou <nn.mochou+github@gmail.com>
2025-12-22 15:02:13 +0800

    refactor(security): 调整GPG代理配置
    移除了`modules/nixos/desktop/base/security.nix`中关于GPG代理的`enable`和`enableSSHSupport`属性，因为在`modules/sharedModule/os/ssh.nix`中已经进行了相应的配置。
    
    在`modules/sharedModule/os/ssh.nix`中添加了GPG代理的相关配置，包括`enable`和`enableSSHSupport`属性，以确保GPG代理和SSH支持的一致性。

--- modules/nixos/desktop/base/security.nix
+++ modules/nixos/desktop/base/security.nix
@@ -63,9 +63,7 @@
 
     # gpg agent with pinentry
     programs.gnupg.agent = {
-      enable = true;
       pinentryPackage = pkgs.pinentry-qt;
-      enableSSHSupport = false;
       settings.default-cache-ttl = 4 * 60 * 60; # 4 hours
     };
 
--- modules/sharedModule/os/ssh.nix
+++ modules/sharedModule/os/ssh.nix
@@ -2,4 +2,12 @@
   services.openssh = {
     enable = true;
   };
+
+  programs.gnupg = {
+    agent = {
+      enable = true;
+      enableSSHSupport = true;
+    };
+  };
+
 }
