d64d9c157ecc31a46c7152fd6cc73df64c599efd
mochou <nn.mochou+github@gmail.com>
2026-01-17 19:18:44 +0800

    chore: 更新ty版本

--- overlays/pkgs/ty.nix
+++ overlays/pkgs/ty.nix
@@ -1,12 +1,12 @@
 self: super: {
   ty = super.ty.overrideAttrs (oldAttrs: rec {
-    version = "0.0.11";
+    version = "0.0.12";
     src = super.fetchFromGitHub {
       owner = "astral-sh";
       repo = "ty";
       tag = version;
       fetchSubmodules = true;
-      hash = "sha256-lelhsc6zl6Qe7W7YsXviqB3NEOuJAzewnOgFPtsCYGA=";
+      hash = "sha256-HbIntp5dhJgR3WdX3mtxhghHo5twQFiGfbHprWSsei8=";
     };
     cargoDeps = super.rustPlatform.importCargoLock {
       lockFile = src + "/ruff/Cargo.lock";
