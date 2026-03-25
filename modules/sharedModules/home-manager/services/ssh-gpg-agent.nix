d7007828c4b8c6e825b2097acb4df8a88f315c07
mochou <nn.mochou+github@gmail.com>
2026-03-26 00:34:51 +0800

    feat(home-manager): use ssh-agent

---
+++ modules/sharedModules/home-manager/services/ssh-gpg-agent.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  config,
+  ...
+}:
+{
+
+  programs.gpg = {
+    enable = false;
+    settings = {
+    };
+  };
+
+  services = {
+    ssh-agent = {
+      enable = true;
+      enableZshIntegration = true;
+      enableBashIntegration = true;
+    };
+    gpg-agent = {
+      enable = false;
+      enableSshSupport = true;
+      enableExtraSocket = true;
+      enableZshIntegration = config.programs.zsh.enable;
+      pinentry.package = if pkgs.stdenv.isDarwin then pkgs.pinentry_mac else pkgs.pinentry-qt;
+      defaultCacheTtlSsh = 3600;
+      defaultCacheTtl = 3600;
+      maxCacheTtl = 7200;
+      maxCacheTtlSsh = 7200;
+      extraConfig = ''
+        allow-loopback-pinentry
+        allow-preset-passphrase
+      '';
+    };
+  };
+
+}
