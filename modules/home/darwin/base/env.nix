89f3dda2c1855f0b3a9fd2b70d580cee0cbea511
mochou <nn.mochou+github@gmail.com>
2026-04-14 11:04:02 +0800

    refactor(linux): remove env.`projects`

--- modules/home/darwin/base/env.nix
+++ modules/home/darwin/base/env.nix
@@ -1,9 +1,4 @@
 {
-  home = {
-    shellAliases = {
-      projects = "cd /Volumes/Code";
-    };
-  };
 
   programs.go = {
     GOPATH = "/Volumes/Code/Projects/golang";
--- modules/home/linux/base/tools/env.nix
+++ modules/home/linux/base/tools/env.nix
@@ -1,12 +1,5 @@
 { config, ... }:
 {
-  programs = {
-    zsh = {
-      dirHashes = {
-        projects = "$HOME/Code";
-      };
-    };
-  };
 
   programs.go = {
     GOPATH = "${config.home.homeDirectory}/Code/Projects/golang";
